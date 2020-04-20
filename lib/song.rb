a1000352891c45fb91698b14dc016d4a658b5209
Jack Cooper <jackcooper1245@gmail.com>
2020-04-20 10:26:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,6 +17,11 @@ def self.new_by_filename(file)
   song
 end
 
+def artist_name=(name)
+  self.artist = Artist.find_or_create_by_name(name)
+  self.artist.add_song(self)
+end
+
 def self.all
   @@all
 end
