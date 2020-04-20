73bcfa6567f9e158cd5776bc88e6a9412561caac
Jack Cooper <jackcooper1245@gmail.com>
2020-04-20 10:53:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-cclass Song
+class Song
   attr_accessor :name, :artist
 
   def initialize(name)
@@ -8,7 +8,7 @@ cclass Song
   def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
-    song = Song.new(name)
+    song = Song.new(song_name)
     song.artist_name = artist_name
     song
   end
