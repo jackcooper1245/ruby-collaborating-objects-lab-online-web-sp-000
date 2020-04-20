dc685baf22371ec079baa2e48bc2da3d983b09dd
Jack Cooper <jackcooper1245@gmail.com>
2020-04-20 10:24:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,11 @@ def initialize(name)
   save
 end
 
-def self.new_by_filename(filename)
-  song = self.new
-  song.title = filename.split(" - ")[1]
+def self.new_by_filename(file)
+  song_name = file.split(" - ")[1]
+  artist = file.split(" - ")[0]
+  song = self.new(song_name)
+  song.artist.name = artist
   song
 end
 
