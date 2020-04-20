fd2ed594a396fc319c0e9d7c841007690304c9d0
Jack Cooper <jackcooper1245@gmail.com>
2020-04-20 10:06:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,4 +23,13 @@ def save
   @@all << self
 end
 
+def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+end
+
 end
